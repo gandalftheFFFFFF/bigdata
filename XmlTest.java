1bb549c1e28caded4739f4d089a328adc52f4723
nicholasjorg <nicholasjorgensen@Nicholass-MacBook-Pro-2.local>
2015-11-03 12:39:58 +0100

    file upload

---
+++ XmlTest.java
@@ -0,0 +1,123 @@
+package docTest;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class XmlTest {
+
+  public static void main(String args[]) {
+	  
+	  HashMap<Double, LinkedList<Data>> mapper;
+	  mapper = new HashMap<>();
+	  
+	  
+	  File file = new File("part-m-00000");
+
+      try{
+          Scanner scanner = new Scanner(file);
+          
+          while(scanner.hasNext()){
+        	  String line = scanner.next();
+        	  String[] arr = line.split(",");
+        	  // 0time, 1type, 2id, 3x, 4y, 5angle, 6speed, 7pos, 8lane, 9edge
+        	 
+        	  Data tmpData = new Data();
+        	  double id = Double.parseDouble(arr[2]);
+        	 
+        	  if(arr[1].equals("v")){
+        		  tmpData.setTime(Double.parseDouble(arr[0]));
+        		  tmpData.setX(Double.parseDouble(arr[3]));
+        		  tmpData.setY(Double.parseDouble(arr[4]));
+        		  tmpData.setSpeed(Double.parseDouble(arr[6]));
+        		  tmpData.setAngle(Double.parseDouble(arr[5]));
+        	  }
+        	  
+        	  if(mapper.containsKey(id)){
+        		  LinkedList<Data> list = mapper.get(id);
+        		  list.add(tmpData);
+        		  mapper.put(id, list);
+        	  }
+        	  else{
+        		  LinkedList<Data> list = new LinkedList<>();
+        		  list.add(tmpData);
+        		  mapper.put(id, list);
+        	  }
+        	  
+        	  
+          }
+          scanner.close();
+          
+      }catch (FileNotFoundException e){
+
+          e.printStackTrace();
+      }
+	  
+     LinkedList<Data> list = mapper.get(1);
+     System.out.println(list.size());
+     ListIterator itr = list.listIterator();
+     while(itr.hasNext()){
+    	 Data data = (Data) itr.next();
+    	 System.out.println(data.getTime());
+     }
+  }
+
+}
+class Data{
+	private int id;
+	private Double time;
+	private double x;
+    private double y;
+    private double angle;
+    private double speed;
+    private double pos;
+    private String lane;
+    private double slope;
+    
+    public void setId(int id){
+    	this.id=id;
+    }
+    public int getId(){
+    	return id;
+    }
+    public void setTime(Double time){
+    	this.time = time;
+    }
+    public double getTime(){
+    	return time;
+    }
+    public void setX(double x){
+    	this.x = x;
+    }
+    public double getX(){
+    	return x;
+    }
+    public void setY(double y){
+    	this.y = y;
+    }
+    public double getY(){
+    	return y;
+    }
+    public void setAngle(double angle){
+    	this.angle = angle;
+    }
+    public double getAngle(){
+    	return angle;
+    }
+    public void setSpeed(double speed){
+    	this.speed = speed;
+    }
+}
